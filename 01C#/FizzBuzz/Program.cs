35d7de102667e27e7df9146f4dde673196f38101
oddishhorde <91993779+oddishhorde@users.noreply.github.com>
2022-06-21 15:31:39 -0500

    gitignore added

---
+++ 01C#/FizzBuzz/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What's your name?");
+
+//string name = Console.ReadLine();
+
+//Console.WriteLine("Hello " + name + "!");
+
+
+int x = 3;
+int y = 5;
+
+int z = x+y;
+Console.WriteLine(z);
+
+//'%' returns the remainder
+Console.WriteLine(4%2);
+Console.WriteLine(6%2);
+
+//truth table
+// && and
+// || or
