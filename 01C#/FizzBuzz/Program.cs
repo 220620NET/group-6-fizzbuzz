8b77d84d90ad7714d9c27cdd4cb3116f0f106254
oddishhorde <91993779+oddishhorde@users.noreply.github.com>
2022-06-21 16:17:11 -0500

    uploaded fizzbuzz project

--- 01C#/FizzBuzz/Program.cs
+++ 01C#/FizzBuzz/Program.cs
@@ -1,22 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("What's your name?");
+Console.WriteLine("Enter a number?");
 
-//string name = Console.ReadLine();
+string stringNum = Console.ReadLine();
+int num = int.Parse(stringNum);
 
-//Console.WriteLine("Hello " + name + "!");
+Console.WriteLine("You entered " + num + "!");
 
 
-int x = 3;
-int y = 5;
+//int x = 3;
+//int y = 5;
 
-int z = x+y;
-Console.WriteLine(z);
+//int z = x+y;
+//Console.WriteLine(z);
 
 //'%' returns the remainder
-Console.WriteLine(4%2);
-Console.WriteLine(6%2);
+//Console.WriteLine(4%2);
+//Console.WriteLine(6%2);
 
 //truth table
 // && and
 // || or
+
+/*
+if(name == "chris")
+{
+    Console.WriteLine("Hi Chris!");
+}
+else
+{
+    Console.WriteLine("You're not Chris!");
+}
+*/
+
+int n = 1;
+
+for(int i=1;i < num+1;i++)
+{
+    if(i%3!=0 && i%5!=0)
+    {
+        Console.WriteLine(i);
+    }
+    else if (i%3==0 && i%5==0)
+    {
+        Console.WriteLine("Fizz Buzz");
+    }
+    else if(i%3==0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if(i%5==0)
+    {
+        Console.WriteLine("Buzz");
+    }
+}
